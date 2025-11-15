083aeb8302d74267bc2e86b3d6fe6d4d941d1864
Pramod <pramodshenkar2210@gmail.com>
2025-11-16 01:57:18 +0530

    implemented singleflight to for cache stamping

--- internal/adapter/outbound/gorm/dao/account.go
+++ internal/adapter/outbound/gorm/dao/account.go
@@ -8,18 +8,24 @@ import (
 	"txn-processor/internal/adapter/outbound/gorm/entity"
 	"txn-processor/internal/core/model"
 	"txn-processor/internal/port"
+
+	"golang.org/x/sync/singleflight"
 )
 
 const accountTTL = 60 * time.Second
 
 type accountDAO struct {
 	*Connections
+	sf singleflight.Group
 }
 
 var _ port.AccountDao = (*accountDAO)(nil)
 
 func NewAccountDAO(conn *Connections) port.AccountDao {
-	return &accountDAO{Connections: conn}
+	return &accountDAO{
+		Connections: conn,
+		sf:          singleflight.Group{},
+	}
 }
 
 func (d *accountDAO) CreateAccount(ctx context.Context, req model.AccountCreateRequest) error {
@@ -34,7 +40,6 @@ func (d *accountDAO) CreateAccount(ctx context.Context, req model.AccountCreateR
 	if err := d.db.WithContext(ctx).
 		Model(&entity.Account{}).
 		Create(&e).Error; err != nil {
-
 		span.RecordError(err)
 		return err
 	}
@@ -45,8 +50,11 @@ func (d *accountDAO) CreateAccount(ctx context.Context, req model.AccountCreateR
 	}
 
 	b, _ := json.Marshal(resp)
-	if err := d.cache.Set(ctx, fmt.Sprintf("account:%d", e.AccountID), b, accountTTL).Err(); err != nil {
+	key := fmt.Sprintf("account:%d", e.AccountID)
+
+	if err := d.cache.Set(ctx, key, b, accountTTL).Err(); err != nil {
 		span.RecordError(err)
+		d.cache.Del(ctx, key).Err()
 	}
 
 	return nil
@@ -64,30 +72,39 @@ func (d *accountDAO) GetAccountByID(ctx context.Context, id int64) (*model.Accou
 		if json.Unmarshal([]byte(val), &cached) == nil {
 			return &cached, nil
 		}
+		span.RecordError(fmt.Errorf("cache unmarshal error for key %s", key))
 	} else {
 		span.RecordError(err)
 	}
 
-	var e entity.Account
+	result, err, _ := d.sf.Do(key, func() (interface{}, error) {
+		var e entity.Account
 
-	if err := d.db.WithContext(ctx).
-		Model(&entity.Account{}).
-		Where("account_id = ?", id).
-		First(&e).Error; err != nil {
+		if err := d.db.WithContext(ctx).
+			Model(&entity.Account{}).
+			Where("account_id = ?", id).
+			First(&e).Error; err != nil {
+			span.RecordError(err)
+			return nil, err
+		}
 
-		span.RecordError(err)
+		resp := &model.AccountGetResponse{
+			AccountID: e.AccountID,
+			Balance:   e.Balance,
+		}
+
+		b, _ := json.Marshal(resp)
+		if err := d.cache.Set(ctx, key, b, accountTTL).Err(); err != nil {
+			span.RecordError(err)
+			d.cache.Del(ctx, key).Err()
+		}
+
+		return resp, nil
+	})
+
+	if err != nil {
 		return nil, err
 	}
 
-	resp := &model.AccountGetResponse{
-		AccountID: e.AccountID,
-		Balance:   e.Balance,
-	}
-
-	b, _ := json.Marshal(resp)
-	if err := d.cache.Set(ctx, key, b, accountTTL).Err(); err != nil {
-		span.RecordError(err)
-	}
-
-	return resp, nil
+	return result.(*model.AccountGetResponse), nil
 }
